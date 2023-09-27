873ff82dce71f9acbf24e1f495a2e68bca21be8a
Tor4ubas <alex1029@yandex.ru>
2023-09-27 16:30:06 +0300

    исправлен скрипт для создания таблиц

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,23 +1,22 @@
 -- SQL-команды для создания таблиц
 CREATE TABLE employees (
-    id INT PRIMARY KEY,
-    first_name VARCHAR(25),
-    last_name VARCHAR(50),
-    title VARCHAR(100),
+    employee_id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(25) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    title VARCHAR(100) NOT NULL,
     birth_date DATE,
     notes text
 );
 CREATE TABLE customers (
-    id INT PRIMARY KEY,
+    customer_id VARCHAR(5) PRIMARY KEY,
     company_name VARCHAR(50),
     contact_name VARCHAR(50)
 );
 CREATE TABLE orders (
-    id INT PRIMARY KEY,
-    customer_id INT,
-    employee_id INT,
+    order_id INT PRIMARY KEY,
+    customer_id VARCHAR(5) REFERENCES customers(customer_id) NOT NULL,
+    employee_id int REFERENCES employees(employee_id) NOT NULL,
     order_date DATE,
     ship_city VARCHAR(50),
-    FOREIGN KEY (customer_id) REFERENCES customers(id),
-    FOREIGN KEY (employee_id) REFERENCES employees(id)
-);
+   UNIQUE(customer_id)
+)
