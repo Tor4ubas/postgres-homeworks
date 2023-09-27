033deab2da388c69d0299b5cb2f96b80adcd6a43
Tor4ubas <alex1029@yandex.ru>
2023-09-27 14:32:16 +0300

    Созданы три таблицы, используя `query tool` в DBeaver     - `employees`     - `customers`     - `orders`

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,23 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(25),
+    last_name VARCHAR(50),
+    title VARCHAR(100),
+    birth_date DATE,
+    notes text
+);
+CREATE TABLE customers (
+    id INT PRIMARY KEY,
+    company_name VARCHAR(50),
+    contact_name VARCHAR(50)
+);
+CREATE TABLE orders (
+    id INT PRIMARY KEY,
+    customer_id INT,
+    employee_id INT,
+    order_date DATE,
+    ship_city VARCHAR(50),
+    FOREIGN KEY (customer_id) REFERENCES customers(id),
+    FOREIGN KEY (employee_id) REFERENCES employees(id)
+);
