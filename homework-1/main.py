1abd0e9330a5801eb1b4f4b96253044bd0cfdc3f
Tor4ubas <alex1029@yandex.ru>
2023-09-27 14:33:09 +0300

    скрипт в `main.py`, который заполнет созданные таблицы данными из `north_data`

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,75 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import psycopg2
+import os
+import csv
+
+create_tables = 'create_tables.sql'
+employees = os.path.abspath('north_data/employees_data.csv')
+orders = os.path.abspath('north_data/orders_data.csv')
+customers = os.path.abspath('north_data/customers_data.csv')
+
+# connect to db
+conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='qwert54321')
+
+
+def main():
+    try:
+        with conn:
+
+            with conn.cursor() as cursor:
+                # execute query
+                cursor.execute(open(create_tables, 'r').read())
+
+                # Заполнение таблицы сотрудников
+                with open(employees, 'r', encoding='utf8') as file:
+                    info = csv.reader(file, delimiter=",")
+                    count = 0
+                    for row in info:
+                        if count == 0:
+                            count += 1
+                            continue
+                        else:
+                            cursor.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)",
+                                           (int(row[0]), row[1], row[2], row[3], row[4], row[5]))
+
+                # Заполнение таблицы клиентов
+                with open(customers, 'r', encoding='utf8') as file:
+                    info = csv.reader(file, delimiter=",")
+                    count = 0
+                    for row in info:
+                        if count == 0:
+                            count += 1
+                            continue
+                        else:
+                            try:
+                                cursor.execute("INSERT INTO customers VALUES (%s, %s, %s)",
+                                               (row[0], row[1], row[2]))
+                                conn.commit()
+                            except psycopg2.DatabaseError:
+                                conn.rollback()
+                                continue
+
+                # Заполнение таблицы заказов
+                with open(orders, 'r', encoding='utf8') as file:
+                    info = csv.reader(file, delimiter=",")
+                    count = 0
+                    for row in info:
+                        if count == 0:
+                            count += 1
+                            continue
+                        else:
+                            try:
+                                cursor.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)",
+                                           (int(row[0]), row[1], int(row[2]), row[3], row[4]))
+                                conn.commit()
+                            except psycopg2.DatabaseError:
+                                conn.rollback()
+                                continue
+
+    finally:
+        # close connection
+        conn.close()
+
+if __name__ == "__main__":
+    main()
