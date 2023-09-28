bdaf3e1df183882e8893afc507273dfd9dc48566
Tor4ubas <alex1029@yandex.ru>
2023-09-28 11:23:37 +0300

    исправлены 1,2 задания (удалены подзапросы). 3-е написан через один из JOINов. 4-е удален EXISTS.

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,52 +1,36 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-SELECT  customer,
-        employee
-FROM  (SELECT customers.company_name AS customer,
-        customer_id,
-        CONCAT(employees.first_name, ' ', employees.last_name) AS employee,
-        employee_id
-    FROM customers,
-        employees
-    WHERE customers.city = 'London' AND employees.city = 'London') AS first_work
-LEFT JOIN orders USING (customer_id, employee_id)
-WHERE ship_via = (SELECT shipper_id
-     FROM shippers
-   WHERE company_name = 'United Package')
-
+SELECT customers.company_name, CONCAT(employees.first_name, ' ', employees.last_name)
+FROM orders
+INNER JOIN customers USING (customer_id)
+INNER JOIN employees USING (employee_id)
+INNER JOIN shippers ON orders.ship_via = shippers.shipper_id
+WHERE customers.city = 'London' AND employees.city = 'London' AND shippers.company_name = 'United Package'
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
-SELECT product_name,
-    units_in_stock,
-    contact_name,
-    phone
-FROM (SELECT products.product_name,
-        products.units_in_stock,
-        products.category_id,
-        products.supplier_id
-    FROM products
-    WHERE products.discontinued != 1 AND products.units_in_stock < 25) AS first_work
-LEFT JOIN suppliers USING (supplier_id)
-WHERE category_id IN (SELECT category_id
-    FROM categories
-    WHERE category_name IN ('Dairy Products', 'Condiments'))
-ORDER BY units_in_stock
+SELECT products.product_name, products.units_in_stock,
+       suppliers.contact_name, suppliers.phone
+FROM products
+JOIN suppliers ON products.supplier_id = suppliers.supplier_id
+JOIN categories ON products.category_id = categories.category_id
+WHERE products.discontinued <> 1 AND products.units_in_stock < 25
+      AND categories.category_name IN ('Dairy Products', 'Condiments')
+ORDER BY products.units_in_stock ASC
+
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
-SELECT company_name
+SELECT customers.company_name
 FROM customers
-WHERE NOT EXISTS (SELECT *
-       FROM orders
-       WHERE customers.customer_id=orders.customer_id)
+LEFT JOIN orders ON customers.customer_id = orders.customer_id
+WHERE orders.customer_id IS NULL
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
-SELECT products.product_name
+SELECT DISTINCT products.product_name
 FROM products
-WHERE EXISTS (SELECT DISTINCT product_id
-    FROM order_details
-    WHERE products.product_id=order_details.product_id AND quantity = 10)
+JOIN order_details ON products.product_id = order_details.product_id
+WHERE order_details.quantity = 10
